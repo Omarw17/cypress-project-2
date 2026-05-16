f448a7a3cd123c9e1d5929e43f99dd9132c19e1e
Omar Walid <omarwalid@Omars-MacBook-Air.local>
2026-05-16 12:54:57 +0300

    fix: remove unused POM content, clean up to 5 features only

--- cypress/support/pom/account-pom.js
+++ cypress/support/pom/account-pom.js
@@ -1,30 +1,3 @@
-class AccountPOM {
-
-    clickEditProfile() {
-        cy.get('[data-test="nav-profile"]', { timeout: 15000 }).should('be.visible').click();
-    }
-
-    updateFirstName(firstName) {
-        cy.get('[data-test="first-name"]', { timeout: 10000 }).clear().type(firstName);
-    }
-
-    updateLastName(lastName) {
-        cy.get('[data-test="last-name"]', { timeout: 10000 }).clear().type(lastName);
-    }
-
-    clickSaveProfile() {
-        cy.get('[data-test="update-profile-submit"]', { timeout: 15000 }).should('be.visible').click();
-    }
-
-    verifyAccountPageVisible() {
-        cy.get('[data-test="nav-menu"]', { timeout: 15000 }).should('be.visible');
-        cy.url().should('include', '/account');
-    }
-
-    verifyOrderHistoryVisible() {
-        cy.get('[data-test="order-history"]', { timeout: 15000 }).should('be.visible');
-    }
-
-}
+class AccountPOM {}
 
 export default AccountPOM;
--- cypress/support/pom/cart-pom.js
+++ cypress/support/pom/cart-pom.js
@@ -1,15 +1,3 @@
-class CartPOM {
-    removeCartItem(itemIndex) {
-        cy.get('tbody .btn-danger', { timeout: 10000 }).eq(itemIndex).click();
-        cy.wait(1500);
-    }
-    verifyCartNotEmpty() {
-        cy.get('[data-test="product-title"]', { timeout: 15000 }).should('have.length.greaterThan', 0);
-        cy.get('[data-test="proceed-1"]').should('exist');
-    }
-    verifyEmptyCart() {
-        cy.contains('The cart is empty. Nothing to display.', { timeout: 15000 }).should('exist');
-        cy.get('[data-test="product-title"]').should('not.exist');
-    }
-}
+class CartPOM {}
+
 export default CartPOM;
